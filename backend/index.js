34a2223d2e0d581cd239203b745a21daa7343b1f
Dagi <dagmawisamson212121@gmail.com>
2025-04-14 19:23:36 -0400

    initial

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import express from "express"
+import mysql from "mysql"
+
+const app = express()
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"flight_tracking"
+})
+
+// db.connect((err) => {
+//     if (err) {
+//         console.error("Error connecting to MySQL:", err.message);
+//         return;
+//     }
+//     console.log("MySQL connected!");
+// })
+
+// app.get("/airplane",(req,res)=>{
+//     const q = "SELECT * FROM ariplane"
+//     db.query(q,(err,data)=>{
+//         if(err) return res.json(err)
+//             return res.json(data)
+//     })
+// })
+app.get("/",(req,res) =>{
+    res.json("Dagmawi Begashaw  in all aspect")
+})
+app.listen(8800,()=>{
+    console.log("Connected to backend!!")
+})
